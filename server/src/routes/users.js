352d1fce681e30f8263ed288b72bfc69fffb5e6c
arpan-semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-04-07 12:05:16 +0530

    login/register

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -9,11 +9,39 @@ const router = express.Router();
 router.post("/register" , async(req , res) =>{ //res : to send data who made the reques
     const {username , password} = req.body;
 
-    const user = await UserModel.findOne({username});
+    const user = await UserModel.findOne({username}); // finding user in database through findONE funciton 
 
-    res.json(user);
+    if(user) { //checking if user already exist in database
+        return res.json({message : "user already exist"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password , 10);
+
+    const newUser = new UserModel({username , password:hashedPassword});// new user created in mongoDB
+    await newUser.save(); //saving the new user 
+    res.json({message: "User Registered Successfully"})
 });
 
-router.post("/login");
+router.post("/login" , async(req  , res) => {
+    const {username , password} = req.body;
+    const user = await UserModel.findOne({username});
+
+    if(!user){
+        return res.json({message: "user does not exit"});
+    }
+    //comparing password , once the password has been hashed it acannot be unhashed 
+    const isPasswordValid = await bcrypt.compare(password , user.password);
+
+    if(!user){
+        return res.json({message:"Username or password is not correct"});
+    }
+
+    //create token 
+    const token = jwt.sign({id: user.id},"secret");
+    res.json({token , userID: user._id})
+
+
+
+});
 
 export {router as userRouter};
